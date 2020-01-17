1e713ea614b077c6230419271d4b3fb285e84342
guojianning <524343790@qq.com>
2020-01-17 17:44:17 +0800

    spring-cloud学习

---
+++ service-zuul/src/main/java/com/springcloud/demo/filter/MyFilter.java
@@ -0,0 +1,66 @@
+package com.springcloud.demo.filter;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.servlet.http.HttpServletRequest;
+
+public class MyFilter extends ZuulFilter {
+
+    private static Logger log = LoggerFactory.getLogger(MyFilter.class);
+
+    /**
+     * pre：可以在请求被路由之前调用
+     * route：在路由请求时候被调用
+     * post：在route和error过滤器之后被调用
+     * error：处理请求时发生错误时被调用
+     * @return
+     */
+    @Override
+    public String filterType() {
+        return "pre";
+    }
+
+    /**
+     * 通过int值来定义过滤器的执行顺序
+     * @return
+     */
+    @Override
+    public int filterOrder() {
+        return 0;
+    }
+
+    /**
+     * 返回一个boolean类型来判断该过滤器是否要执行，所以通过此函数可实现过滤器的开关
+     * @return
+     */
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public Object run() throws ZuulException {
+        RequestContext ctx = RequestContext.getCurrentContext();
+        HttpServletRequest request = ctx.getRequest();
+        log.info(String.format("%s >>> %s", request.getMethod(), request.getRequestURL().toString()));
+        Object accessToken = request.getParameter("token");
+        if(accessToken == null) {
+            log.warn("token is empty");
+            ctx.setSendZuulResponse(false);
+            ctx.setResponseStatusCode(401);
+            try {
+                ctx.getResponse().getWriter().write("token is empty");
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+
+            return null;
+        }
+        log.info("ok");
+        return null;
+    }
+}
