ac287e982d8780658e87f12d43156659e1730ac7
guojianning <524343790@qq.com>
2020-09-29 17:09:42 +0800

    添加springcloud gateway

--- service-zuul/src/main/java/com/springcloud/demo/filter/MyFilter.java
+++ service-zuul/src/main/java/com/springcloud/demo/filter/MyFilter.java
@@ -5,9 +5,11 @@ import com.netflix.zuul.context.RequestContext;
 import com.netflix.zuul.exception.ZuulException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
 
 import javax.servlet.http.HttpServletRequest;
 
+@Component
 public class MyFilter extends ZuulFilter {
 
     private static Logger log = LoggerFactory.getLogger(MyFilter.class);
